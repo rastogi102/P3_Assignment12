1c92a4e64d07f50be96dbbb59e62832eb68f80c9
rastogi102 <nikhilrastogi102@gmail.com>
2023-08-29 14:28:53 +0530

    initial commit

---
+++ WebApplication/WebApplication/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+        public string PublicationDate { get; set; }
+        public string PostId { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PublicationDate { get; set; }
+        public string Author { get; set; }
+    }
+}
